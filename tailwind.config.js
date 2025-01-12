4a451b3f0d3da65b2914beed88b4c4b4a29382cc
Mayuresh Juvekar <73056290+Mayureshju@users.noreply.github.com>
2025-01-12 17:24:32 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,22 @@ export default {
   ],
   darkMode: 'class',
   theme: {
-    extend: {},
+ extend: {
+      keyframes: {
+        "fade-in": {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+        "slide-in-from-bottom": {
+          "0%": { transform: "translateY(20px)" },
+          "100%": { transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        "fade-in": "fade-in 0.3s ease-out",
+        "slide-in-from-bottom": "slide-in-from-bottom 0.3s ease-out",
+      },
+    },
   },
   plugins: [],
 }
